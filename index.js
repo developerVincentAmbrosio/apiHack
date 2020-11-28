c36a4a744646409dd0645907626400df36339ccf
developerVincentAmbrosio <70174185+developerVincentAmbrosio@users.noreply.github.com>
2020-11-27 21:55:20 -0500

    finished styles & added alert for results fetch

--- index.js
+++ index.js
@@ -1,99 +1,124 @@
 'use strict';
 
+//const { forEach } = require("async");
+
 const baseUrl = "https://recipe-puppy.p.rapidapi.com/";
-const host = "recipe-puppy.p.rapidapi.com"
+const host = "recipe-puppy.p.rapidapi.com";
 const apiKey = "65ecb5ffaamshaec6ef4d281b1d8p126c8fjsn4e72a949ea28";
 
-//"https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet"
+const options = {
+    headers: new Headers({
+        "x-rapidapi-key": apiKey,
+        "x-rapidapi-host": host,
+        "useQueryString": true
+    })      
+};
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .map(key => {
+            if (params[key] !== "") {
+                return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+            }
+        })
     return queryItems.join('&');
 }
 
-function getRecipes(query) {
+function getRandomRecipeData() {
+    fetch(baseUrl + `?p=${Math.floor(Math.random()*(10 - 1) + 1)}&onlyImages=1`, options)
+        .then(response => response.json())
+        .then(responseJson => displayRandomRecipeOfTheDay(responseJson));
+};
+
+function getRecipes(userRecipeInput, userIngredientInput) {
     const params = {
-        q: query
-    };
+        q: userRecipeInput,
+        i: userIngredientInput
+    };   
 
     const queryString = formatQueryParams(params)
-    const url = baseUrl + '?' + queryString;
-
-    const options = {
-        headers: new Headers({
- 		    "x-rapidapi-key": apiKey,
-            "x-rapidapi-host": host,
-            "useQueryString": true })
-    };
+    const url = baseUrl + '?' + queryString + '&onlyImages=1';
 
     fetch(url, options)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            alert('Please try another recipe or ingredient.');
+        })    
         .then(responseJson => displayResults(responseJson));
 }
 
-// function displayRandomRecipeOfTheDay(responseJson) {
-//    for (let i = 0; i < responseJson.results.length; i++) {
-//         const randomRecipe = responseJson[Math.floor(Math.random()*responseJson.length)];
-//         console.log(randomRecipe);
-//         const RecipeOfTheDay = Object.create(randomRecipe);
-//         RecipeOfTheDay.thumbnail
+function displayRandomRecipeOfTheDay(responseJson) {
+        const randomRecipe = responseJson.results[Math.floor(Math.random()*responseJson.results.length)];
+        const radnomRecipeIngredients = randomRecipe.ingredients.split(",");
+        let radnomRecipeIngreditentsList = "";
 
-//         ]
-//         $('#js-random-daily-recipe').append(`
-//             <div class="double">
-//                 <img src="${randomRecipeOfTheDay[i].thumbnail}" alt="${randomRecipeOfTheDay[i].title}">
-//                 <h3>${randomRecipeOfTheDay[i].title}</h3>
-//                 <a href="${randomRecipeOfTheDay[i].href}" target="_blank">link to recipe</a>
-//             </div>
-//             <div class="double">
-//                 <h3>Ingredients:</h3>
-//                 <p>${randomRecipeOfTheDay.ingredients}</p>
-//             </div>
-//         `)
-//    };
-// }
+        radnomRecipeIngredients.forEach((ingredient,i) => {
+            radnomRecipeIngreditentsList += `<li>${ingredient}</li>`
+        })
+
+        $('#js-random-daily-recipe').append(`
+            <div class="item dailyrecipe">
+            <h3>${randomRecipe.title}</h3>
+                <img src="${randomRecipe.thumbnail}" alt="${randomRecipe.title}">
+            </div>
+            <div class="item">
+                <h4>Ingredients:</h4>
+                <ul>${radnomRecipeIngreditentsList}</ul>
+                <hr>
+                <a href="${randomRecipe.href}" target="_blank">link to recipe</a>
+            </div>
+            
+        `)
+}
   
 function displayResults(responseJson) {
     console.log(responseJson);
-    $('#js-recipe-search').empty();
-    for (let i = 0; i < responseJson.results.length; i++) {            
+    $('#js-recipe-results').empty();
+    for (let i = 0; i < responseJson.results.length; i++) {
+        
+        // let recipeIngredients = responseJson.results[i].ingredients.split(",");
+        // let ingredientList = "";
+
+        // recipeIngredients.forEach((ingredient,i) => {
+        //     ingredientList += `<li>${ingredient}</li>` 
+        // })
+
+        //GOES UNDER THE LINK:
+        //<h4>Ingredients: </h4>
+        //<ul>${ingredientList}</ul>
+        
         $('#js-recipe-results').append(`
-            <div class="double">
-                <img src="${responseJson.results[i].thumbnail}" alt="${responseJson.results[i].title}">
-                <h3>${responseJson.results[i].title}</h3>
-                <a href="${responseJson.results[i].href}" target="_blank">link to recipe</a>
+            <div class="recipe-result">
+                <div class="item">
+                    <img src="${responseJson.results[i].thumbnail}" alt="${responseJson.results[i].title}">
+                </div>
+                <div class="item">
+                    <h3>${responseJson.results[i].title}</h3>
+                    <a href="${responseJson.results[i].href}" target="_blank">link to recipe</a>
+                    <p><b>Ingredients:</b> ${responseJson.results[i].ingredients}</p>
+                <div>    
             </div>
-            <div class="double">
-            <h3>Ingredients:</h3>
-            <p>${responseJson.results[i].ingredients}</p>
-            </div>
-        `)   
-    };    
+        `) 
+};  
     $('#js-recipe-results-container').removeClass("hidden")
+    $('#js-recipe-o-day').addClass("hidden")
 }
-
-// function displayIngredients(responseJson) {
-//     let recipeIngredients = [];
-//     for (let i = 0; i < responseJson.results.length; i++) {
-//         recipeIngredients = responseJson.results[i].ingredients.split(",")
-//     };
-//     return recipeIngredients;
-// }
-
 
 function submitFormListner() {
     $('#submit').click(event => {
         event.preventDefault();
         const userRecipeInput = $('#js-recipe-search').val();
-        getRecipes(userRecipeInput);
+        const userIngredientInput = $('#js-ingredient-search').val();
+        getRecipes(userRecipeInput, userIngredientInput);
     })
         
 }
 
 $(function() {
     submitFormListner();
-//    displayRandomRecipeOfTheDay();
+    getRandomRecipeData();
 });
 
 // fetch("https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet", {
